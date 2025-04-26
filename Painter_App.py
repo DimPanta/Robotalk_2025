fd17541e62ca2652235c55b107229780d8eda0ac
DimPanta <dimpantam@gmail.com>
2025-04-26 14:36:14 +0300

    SFHMMY

--- Painter_App.py
+++ Painter_App.py
@@ -10,7 +10,7 @@ import serial.tools.list_ports
 def find_arduino():
     ports = serial.tools.list_ports.comports()
     for port in ports:
-        if "Arduino" in port.description or "CH340" in port.description or "USB Serial Port (COM":
+        if "Arduino" in port.description or "CH340" in port.description or "USB Serial Port (COM" or "/dev/ttyUSB":
             return port.device  # Example: "COM3" on Windows or "/dev/ttyUSB0" on Linux
     return None
 
